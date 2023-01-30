abc8917146b3daa97b9105523246b4e39a45b229
Ian McKechnie <imckechn@uoguelph.ca>
2023-01-30 15:48:28 -0500

    Added in a distribution plot with 2 standard deviations for viewing the normal curve.

--- src/growthRates.py
+++ src/growthRates.py
@@ -7,6 +7,10 @@ import pandas as pd
 from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import helpers
+import plotly.figure_factory as ff
+import numpy as np
+import plotly.graph_objects as go
+
 
 def str2frame(estr, source, sep = ',', lineterm = '\n'):
     dat = [x.split(sep) for x in estr.split(lineterm)][1:-1]
@@ -16,146 +20,113 @@ def str2frame(estr, source, sep = ',', lineterm = '\n'):
         df = pd.DataFrame(dat, columns=["country", 'year', 'species', 'population', "source"] )
     return df
 
+
+ # Step one: Get FAO data
 countries = ["Ethiopia", "Canada", "USA", "Ireland", "India", "Brazil", "Botswana", "Egypt", "South Africa", "Indonesia", "China", "Australia", "NewZealand", "Japan", "Mexico", "Argentina", "Chile"]
 species = ["Cattle","Sheep","Goats","Pigs","Chickens"]
+specie = "Cattle"
+country = "USA"
 
-#Build a Plotly graph around the data
-app = Dash(__name__)
+if specie == None:
+    specie = species[0]
 
-app.layout = html.Div(children=[
-    html.H1(children='Data Quality Comparison for FAO, OIE, Census Data, and National Sources'),
+if country == "USA":
+    fao_data = fao.get_data("United%20States%20of%20America", specie)
 
-    dcc.Graph(id='graph'),
+elif country == None:
+    fao_data = fao.get_data(countries[0], specie)
 
-    html.H3(children='Countries'),
-    dcc.Dropdown(
-        countries,
-        value=countries[0],
-        id="country_checklist",
-    ),
+else:
+    fao_data = fao.get_data(country, specie)
 
-    html.H3(children='Species'),
-    dcc.Dropdown(
-        species,
-        value=species[0],
-        id="species_checklist",
-    ),
-])
+fao_data = str2frame(fao_data, "fao")
+fao_data['source'] = "fao"
+fao_data = fao_data.drop(columns=['iso3', "country"])
+fao_data = fao_data.replace('"','', regex=True)
+fao_data.sort_values(by=['year'], inplace=True)
+
+# Step two: Get OIE data
+if country == "USA":
+    oie_data = oie.get_data("United%20States%20of%20America", specie)
+else:
+    oie_data = oie.get_data(country, specie)
+oie_data = str2frame(oie_data, "oie")
+oie_data['source'] = "oie"
+oie_data = oie_data.drop(columns=['country'])
+oie_data = oie_data.replace('"','', regex=True)
+oie_data.sort_values(by=['year'], inplace=True)
+
+# Step 3: Get Census data
+try:
+    csv_data = pd.read_csv(f"censusData/{country}.csv")
+    species = species + csv_data["species"].tolist() # Add the species from the csv file to the list of species
+    species = list(dict.fromkeys(species))  # Remove duplicates from the list of species
+    csv_index_list = csv_data[(csv_data['species'] == specie)].index.tolist()
+
+except:
+    print("Error, count not find the correct csv file")
+    csv_data = pd.DataFrame()
+    csv_index_list = []
+
+# Step 4: Get National data
+try:
+    nationalData = pd.read_csv(f"nationalData/{country}.csv")
+    #Add the species from the national data
+    species = species + nationalData["species"].tolist() # Add the species from the csv file to the list of species
+    species = list(dict.fromkeys(species))  # Remove duplicates from the list of species
+    nationalData_index_list = nationalData[(nationalData['species'] == specie)].index.tolist()
+
+except:
+    print("Error, count not find the correct csv file")
+    nationalData = pd.DataFrame()
+    nationalData_index_list = []
+
+FaoGrowthRate = helpers.getGrowthRate(fao_data)
+
+FaoGrowthRate.sort_values(by=['growthRate'], inplace=True)
+
+x = np.random.normal( 0, 1, len(FaoGrowthRate['growthRate'].tolist()))
+
+print(x)
+
+hist_data = [x]
+
+print(FaoGrowthRate['growthRate'].tolist())
+data = FaoGrowthRate['growthRate'].tolist()
+
+fig = ff.create_distplot([x], ["FAO"])
+#fig = px.histogram(FaoGrowthRate, x="year", y="growthRate", marginal="box", hover_data=FaoGrowthRate.columns)
+
+#Add the mean and standard deviation to the graph
+mean = np.mean(x)
+stdev_pluss = np.std(x)
+stdev_minus = np.std(x)*-1
+mean = np.mean(x)
+stdev_pluss2 = np.std(x) * 2
+stdev_minus2 = np.std(x)*-1 * 2
+stdev_pluss3 = np.std(x) * 3
+stdev_minus3 = np.std(x)*-1 * 3
 
 
-@app.callback(
-    Output("species_checklist", "options"),
-    Input("country_checklist", "value"))
-def update_species_checklist(country):
-    # Step one: Get FAO data
-    species = ["Cattle","Sheep","Goats","Pigs","Chickens"]
+fig.add_shape(type="line",x0=mean, x1=mean, y0 =0, y1=0.4 , xref='x', yref='y',
+               line = dict(color = 'blue', dash = 'dash'))
+fig.add_shape(type="line",x0=stdev_pluss, x1=stdev_pluss, y0 =0, y1=0.4 , xref='x', yref='y',
+               line = dict(color = 'red', dash = 'dash'))
+fig.add_shape(type="line",x0=stdev_minus, x1=stdev_minus, y0 =0, y1=0.4 , xref='x', yref='y',
+               line = dict(color = 'red', dash = 'dash'))
+fig.add_shape(type="line",x0=stdev_pluss2, x1=stdev_pluss2, y0 =0, y1=0.4 , xref='x', yref='y',
+               line = dict(color = 'Green', dash = 'dash'))
+fig.add_shape(type="line",x0=stdev_minus2, x1=stdev_minus2, y0 =0, y1=0.4 , xref='x', yref='y',
+               line = dict(color = 'Green', dash = 'dash'))
 
-    # Step 3: Get Census data
-    try:
-        csv_data = pd.read_csv(f"censusData/{country}.csv")
-        species = species + csv_data["species"].tolist() # Add the species from the csv file to the list of species
-        species = list(dict.fromkeys(species))  # Remove duplicates from the list of species
-
-    except:
-        print("Error, count not find the correct csv file")
-
-    # Step 4: Get National data
-    try:
-        nationalData = pd.read_csv(f"nationalData/{country}.csv")
-        species = species + nationalData["species"].tolist() # Add the species from the csv file to the list of species
-        species = list(dict.fromkeys(species))  # Remove duplicates from the list of species
-
-    except:
-        print("Error, count not find the correct csv file")
+fig.show()
 
 
-    return species
+# Get the values outside of the second standard deviation
+outliers = []
+for i in range(len(x)):
+    if x[i] > stdev_pluss2 or x[i] < stdev_minus2:
+        outliers.append(x[i])
 
-@app.callback(
-    Output("graph", "figure"),
-    Input("species_checklist", "value"),
-    Input("country_checklist", "value"))
-def update_line_chart(specie, country):
-    # Step one: Get FAO data
-    countries = ["Ethiopia", "Canada", "USA", "Ireland", "India", "Brazil", "Botswana", "Egypt", "South Africa", "Indonesia", "China", "Australia", "NewZealand", "Japan", "Mexico", "Argentina", "Chile"]
-    species = ["Cattle","Sheep","Goats","Pigs","Chickens"]
-
-    if specie == None:
-        specie = species[0]
-
-    if country == "USA":
-        fao_data = fao.get_data("United%20States%20of%20America", specie)
-
-    elif country == None:
-        fao_data = fao.get_data(countries[0], specie)
-
-    else:
-        fao_data = fao.get_data(country, specie)
-
-    fao_data = str2frame(fao_data, "fao")
-    fao_data['source'] = "fao"
-    fao_data = fao_data.drop(columns=['iso3', "country"])
-    fao_data = fao_data.replace('"','', regex=True)
-    fao_data.sort_values(by=['year'], inplace=True)
-
-    # Step two: Get OIE data
-    if country == "USA":
-        oie_data = oie.get_data("United%20States%20of%20America", specie)
-    else:
-        oie_data = oie.get_data(country, specie)
-    oie_data = str2frame(oie_data, "oie")
-    oie_data['source'] = "oie"
-    oie_data = oie_data.drop(columns=['country'])
-    oie_data = oie_data.replace('"','', regex=True)
-    oie_data.sort_values(by=['year'], inplace=True)
-
-    # Step 3: Get Census data
-    try:
-        csv_data = pd.read_csv(f"censusData/{country}.csv")
-        species = species + csv_data["species"].tolist() # Add the species from the csv file to the list of species
-        species = list(dict.fromkeys(species))  # Remove duplicates from the list of species
-        csv_index_list = csv_data[(csv_data['species'] == specie)].index.tolist()
-
-    except:
-        print("Error, count not find the correct csv file")
-        csv_data = pd.DataFrame()
-        csv_index_list = []
-
-    # Step 4: Get National data
-    try:
-        nationalData = pd.read_csv(f"nationalData/{country}.csv")
-        #Add the species from the national data
-        species = species + nationalData["species"].tolist() # Add the species from the csv file to the list of species
-        species = list(dict.fromkeys(species))  # Remove duplicates from the list of species
-        nationalData_index_list = nationalData[(nationalData['species'] == specie)].index.tolist()
-
-    except:
-        print("Error, count not find the correct csv file")
-        nationalData = pd.DataFrame()
-        nationalData_index_list = []
-
-    FaoGrowthRate = helpers.getGrowthRate(fao_data)
-    print(FaoGrowthRate.head())
-
-    FaoGrowthRate.sort_values(by=['growthRate'], inplace=True)
-
-    # Build the plotly graph
-    fig = px.histogram(
-                FaoGrowthRate,
-                x=FaoGrowthRate["year"],
-                y=FaoGrowthRate["growthRate"],
-                #color=master_df["source"],
-                #markers=True)
-    )
-    #fig.update_yaxes(type='linear')
-
-    # fig.update_layout(
-    #     title=f"GrowthRate of the Population in for FAO",
-    #     xaxis_title="Year",
-    #     yaxis_title="Population",
-    #     legend_title="Sources",
-    # )
-    return fig
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+fig2 = go.Figure(data=[go.Table(header=dict(values=['Outliers']),)])
+#fig2.show()
