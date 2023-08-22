bcb22dc0e00547bf3bb84b3e3b7806e576fde96f
Ian McKechnie <imckechn@uoguelph.ca>
2023-08-21 21:02:35 -0400

    Added polynomial regression to a working tab

---
+++ src/test.py
@@ -0,0 +1,531 @@
+# Using Polynomial Regression to Predict Animal Populations
+
+import sys
+sys.path.append('../../src')
+from sklearn.linear_model import LinearRegression
+import API_helpers.fao as fao
+import API_helpers.woah as woah
+import API_helpers.helperFunctions
+import pandas as pd
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
+import plotly.graph_objects as go
+import API_helpers.helperFunctions as helperFunctions
+
+
+countries = ["Ethiopia", "Canada", "USA", "Ireland", "India", "Brazil", "Botswana", "Egypt", "South Africa", "Indonesia", "China", "Australia", "NewZealand", "Japan", "Mexico", "Argentina", "Chile"]
+species   = ["Cattle", "Sheep", "Goats", "Pigs", "Chickens"]
+sources   = ['No Options Available']
+specie    = "Cattle"
+country   = "USA"
+
+#5 year avg
+def groupBy5Years(data, startYear, endYear, type):
+    # Group the data into 5 year intervals
+    sum = 0
+    counter = 0
+    for year in range(int(startYear), int(endYear)):
+        try:
+            if type == "String":
+                row = data[data['year'] == year]
+            else:
+                row = data[data['year'] == year]
+
+            sum += int(row['population'])
+            counter += 1
+
+        except:
+            continue
+
+    if counter == 0:
+        return 0
+    return sum/counter
+
+
+#Build a Plotly graph around the data
+app = Dash(__name__)
+app.config["suppress_callback_exceptions"] = True
+app.title = "GBADs Informatics User Vizualizer"
+
+app.layout = html.Div(children=[
+
+    dcc.Tabs(id="tabs", value='polyRegress', children=[
+        dcc.Tab(label='Polynomial Regression', value='polyRegress'),
+        dcc.Tab(label='Five-Year Population Avg.', value='fiveYearAvg'),
+    ]),
+    html.Br(),
+    html.Div(id='contents'),
+])
+
+@app.callback(
+        Output('contents', 'children'),
+        Input('tabs', 'value'))
+def render_content(tab):
+    print("here")
+    if tab == 'polyRegress':
+        print("In da if statement")
+        return html.Div([
+            html.H1(children='Data Quality Comparison for FAOSTAT, WOAH, Census Data, and National Sources'),
+
+            dcc.Graph(id='graph'),
+
+            html.H3(children='Countries'),
+            dcc.Dropdown(
+                countries,
+                value=countries[0],
+                id="country_checklist",
+            ),
+
+            html.H3(children='Species'),
+            dcc.Dropdown(
+                species,
+                value=species[0],
+                id="species_checklist",
+            ),
+
+            html.H3(children='Source for Prediction'),
+            dcc.Dropdown(
+                sources,
+                value=sources[0],
+                id="sources_checklist",
+            ),
+
+            html.Div([
+                html.Div([
+                    html.H3(children='Degree of Polynomial Regression (>1)'),
+                    dcc.Input(id='polyDegree', value='2', type='number'),
+                ],style={'width': '49%', 'display': 'inline-block'}),
+
+                html.Div([
+                    html.H3(children='Year to be predicted to'),
+                dcc.Input(id='maxYear', value='2050', type='number'),
+                ],style={'width': '49%', 'display': 'inline-block'}),
+            ])
+        ])
+
+    elif tab == 'fiveYearAvg':
+        # Step one: Get FAO Data and WOAH Data
+        # if country == "USA":
+        #     fao_data = fao.get_data("United%20States%20of%20America", specie)
+        #     woah_data = woah.get_data("United%20States%20of%20America", specie)
+        # else:
+        #     fao_data = fao.get_data(country, specie)
+        #     woah_data = woah.get_data(country, specie)
+
+        # fao_data = fao.formatFAOData(fao_data)
+        # woah_data = woah.formatWoahData(woah_data)
+
+        # # Step 3: Get Census Data
+        # csv_data, csv_index_list, species = helperFunctions.getFormattedCensusData(country, specie, species)
+
+        # #Only get the rows that have the correct specie
+        # new_csv_data = []
+        # for index, row in csv_data.iterrows():
+        #     if row['species'] == specie:
+        #         new_csv_data.append( [row['year'], row['population']] )
+
+        # csv_data = pd.DataFrame(new_csv_data, columns = ["year", "population"])
+
+        # # Step 4: Get National Data
+        # nationalData, nationalData_index_list, species = helperFunctions.getFormattedNationalData(country, specie, species)
+
+        # new_national_data = []
+        # for index, row in nationalData.iterrows():
+        #     if row['species'] == specie:
+        #         new_national_data.append( [row['year'], row['population']] )
+
+        # nationalData = pd.DataFrame (new_national_data, columns = ["year", "population"])
+
+
+        # #Get a list of all the years from each data source
+        # fao_years = fao_data['year'].tolist()
+        # woah_years = woah_data['year'].tolist()
+        # csv_years = csv_data['year'].tolist()
+        # national_years = nationalData['year'].tolist()
+
+        # #Combine the years into a set and sort them
+        # years = fao_years + woah_years + csv_years + national_years
+        # years = list(dict.fromkeys(years))
+        # years = [int(s) for s in years]
+        # years.sort()
+
+        # #Get each ones five year averages
+        # fao_averages = []
+        # woah_averages = []
+        # csv_averages = []
+        # national_averages = []
+        # yearsArr = []
+
+        # if len(years) == 0:
+        #     print("No data found")
+        #     exit()
+
+        # counter = 0
+        # for i in range(years[0], years[-1]):
+        #     if counter % 5 == 0 and counter != 0:
+        #         fao_averages.append(groupBy5Years(fao_data, i - 5, i, "String"))
+        #         woah_averages.append(groupBy5Years(woah_data, i - 5, i, "String"))
+        #         csv_averages.append(groupBy5Years(csv_data, i - 5, i, "Int"))
+        #         national_averages.append(groupBy5Years(nationalData, i - 5, i, "Int"))
+        #         yearsArr.append(i)
+
+        #     counter += 1
+
+        # #Find the percent changes
+        # fao_percent_change = []
+        # woah_percent_change = []
+        # csv_percent_change = []
+        # national_percent_change = []
+        # yearsArr.pop(0)
+
+        # for i in range(1, len(fao_averages)):
+        #     if fao_averages[i-1] == 0 or fao_averages[i] == 0:
+        #         fao_percent_change.append(0)
+        #     else:
+        #         fao_percent_change.append( ((fao_averages[i] - fao_averages[i-1]) / fao_averages[i-1]) * 100 )
+
+        #     if woah_averages[i-1] == 0 or woah_averages[i] == 0:
+        #         woah_percent_change.append(0)
+        #     else:
+        #         woah_percent_change.append( ((woah_averages[i] - woah_averages[i-1]) / woah_averages[i-1]) * 100 )
+
+        #     if csv_averages[i-1] == 0 or csv_averages[i] == 0:
+        #         csv_percent_change.append(0)
+        #     else:
+        #         csv_percent_change.append( ((csv_averages[i] - csv_averages[i-1]) / csv_averages[i-1]) * 100 )
+
+        #     if national_averages[i-1] == 0 or national_averages[i] == 0:
+        #         national_percent_change.append(0)
+        #     else:
+        #         national_percent_change.append( ((national_averages[i] - national_averages[i-1]) / national_averages[i-1]) * 100 )
+
+        # #Graph them
+        # masterDf = pd.DataFrame(columns = ["year", "faostat", "WOAH", "census", "national",])
+        # masterDf['FAOSTAT'] = fao_percent_change
+        # masterDf['WOAH'] = woah_percent_change
+        # masterDf['census'] = csv_percent_change
+        # masterDf['national'] = national_percent_change
+        # masterDf['year'] = yearsArr
+
+        # # Census and national are all zeros
+        # if masterDf['census'].isnull().values.any() and masterDf['national'].isnull().values.any():
+
+        #     fig = go.Figure([
+        #         go.Bar(name='FAOSTAT', x=masterDf['year'], y=masterDf['FAOSTAT']),
+        #         go.Bar(name='WOAH', x=masterDf['year'], y=masterDf['WOAH'])
+        #     ])
+
+        # #National is all zeros
+        # elif masterDf['national'].isnull().values.any():
+        #     fig = go.Figure([
+        #         go.Bar(name='FAOSTAT', x=masterDf['year'], y=masterDf['FAOSTAT']),
+        #         go.Bar(name='National', x=masterDf['year'], y=masterDf['national']),
+        #         go.Bar(name='WOAH', x=masterDf['year'], y=masterDf['WOAH']),
+        #     ])
+
+        # #Census is all zeros
+        # elif masterDf['census'].isnull().values.any():
+        #     fig = go.Figure([
+        #         go.Bar(name='FAOSTAT', x=masterDf['year'], y=masterDf['FAOSTAT']),
+        #         go.Bar(name='Census', x=masterDf['year'], y=masterDf['census']),
+        #         go.Bar(name='WOAH', x=masterDf['year'], y=masterDf['WOAH']),
+        #     ])
+
+        # else:
+        #     fig = go.Figure([
+        #         go.Bar(name='FAOSTAT', x=masterDf['year'], y=masterDf['FAOSTAT']),
+        #         go.Bar(name='Census', x=masterDf['year'], y=masterDf['census']),
+        #         go.Bar(name='National', x=masterDf['year'], y=masterDf['national']),
+        #         go.Bar(name='WOAH', x=masterDf['year'], y=masterDf['WOAH']),
+        #     ])
+
+        # fig.update_layout(
+        #     xaxis = dict(
+        #         tickmode='array',
+        #         tickvals = yearsArr,
+        #         ticktext = yearsArr,
+        #     ),
+        #     font=dict(
+        #         color="black",
+        #         size=18
+        #     ),
+        #     plot_bgcolor='white',
+        # )
+
+        # fig.update_yaxes(
+        #     type='linear',
+        #     mirror=True,
+        #     ticks='outside',
+        #     showline=True,
+        #     linecolor='black',
+        #     gridcolor='lightgrey',
+        #     title="Percent Change"
+        # )
+
+        # fig.update_xaxes(
+        #     mirror=True,
+        #     ticks='outside',
+        #     showline=True,
+        #     linecolor='black',
+        #     gridcolor='lightgrey',
+        #     title="Year",
+        #     nticks=len(yearsArr)
+        # )
+
+        # return html.Div([
+        #     html.H1(children='Five Year Average Percent Change in Population of ' + specie + ' in ' + country),
+
+        #     dcc.Graph(id='graph', figure=fig),
+
+        #     html.H3(children='Countries'),
+        #     dcc.Dropdown(
+        #         countries,
+        #         value=countries[0],
+        #         id="country_checklist",
+        #     ),
+
+        #     html.H3(children='Species'),
+        #     dcc.Dropdown(
+        #         species,
+        #         value=species[0],
+        #         id="species_checklist",
+        #     ),
+        # ])
+
+        return html.Div([
+            html.H1(children='In else statement'),
+        ])
+
+    else:
+        print("In da else statement")
+        return html.Div([
+            html.H1(children='In else statement'),
+        ])
+
+@app.callback(
+    Output("species_checklist", "options"),
+    Input("country_checklist", "value"))
+def update_species_checklist(country):
+    # Step one: Get FAO data
+    species = ["Cattle","Sheep","Goats","Pigs","Chickens"]
+
+    # Step 3: Get Census data
+    try:
+        csv_data = pd.read_csv(f"censusData/{country}.csv")
+        species = species + csv_data["species"].tolist() # Add the species from the csv file to the list of species
+        species = list(dict.fromkeys(species))  # Remove duplicates from the list of species
+
+    except:
+        print("Error, count not find the correct csv file")
+
+    # Step 4: Get National data
+    try:
+        nationalData = pd.read_csv(f"nationalData/{country}.csv")
+        species = species + nationalData["species"].tolist() # Add the species from the csv file to the list of species
+        species = list(dict.fromkeys(species))  # Remove duplicates from the list of species
+
+    except:
+        print("Error, count not find the correct csv file")
+
+    return species
+
+
+@app.callback(
+    Output("sources_checklist", "options"),
+    Input("species_checklist", "value"),
+    Input("country_checklist", "value")
+)
+def populate_dropDown(specie, country):
+    species = ["Cattle","Sheep","Goats","Pigs","Chickens"]
+
+    if specie == None:
+        specie = species[0]
+
+    if country == "USA":
+        fao_data = fao.get_data("United%20States%20of%20America", specie)
+
+    elif country == None:
+        fao_data = fao.get_data("Ethiopia", specie)
+
+    else:
+        fao_data = fao.get_data(country, specie)
+
+    fao_data = fao.formatFAOData(fao_data)
+
+    # Step two: Get woah data
+    if country == "USA":
+        woah_data = woah.get_data("United%20States%20of%20America", specie)
+    else:
+        woah_data = woah.get_data(country, specie)
+
+    woah_data = woah.formatWoahData(woah_data)
+
+    # Step 3: Get Census data
+    csv_data, csv_index_list, species  = API_helpers.helperFunctions.getFormattedCensusData(country, specie, species)
+
+    # Step 4: Get National data
+    nationalData, nationalData_index_list, species = API_helpers.helperFunctions.getFormattedNationalData(country, specie, species)
+
+    #Fill the dropdown with the available sources
+    sources = []
+    # if fao_data.empty == False:
+    sources.append("FAOSTAT")
+
+    # if woah_data.empty == False:
+    sources.append("WOAH")
+
+    # if csv_data.empty == False:
+    sources.append("UN Census Data")
+
+    # if nationalData.empty == False:
+    sources.append("National Census Data")
+
+    if sources == []:
+        sources = ["No Options Available"]
+
+    return sources
+
+
+@app.callback(
+    Output("graph", "figure"),
+    Input("species_checklist", "value"),
+    Input("country_checklist", "value"),
+    Input("sources_checklist", "value"),
+    Input("polyDegree", "value"),
+    Input("maxYear", "value"),
+)
+def polynomialRegression(specie, country, source, polyDegree, maxYear):
+    maxYear = int(maxYear)
+
+    # Step one: Get FAO data
+    countries = ["Ethiopia", "Canada", "USA", "Ireland", "India", "Brazil", "Botswana", "Egypt", "South Africa", "Indonesia", "China", "Australia", "NewZealand", "Japan", "Mexico", "Argentina", "Chile"]
+    species = ["Cattle","Sheep","Goats","Pigs","Chickens"]
+
+    if specie == None:
+        specie = species[0]
+
+    if country == "USA":
+        fao_data = fao.get_data("United%20States%20of%20America", specie)
+
+    elif country == None:
+        fao_data = fao.get_data(countries[0], specie)
+
+    else:
+        fao_data = fao.get_data(country, specie)
+
+    fao_data = fao.formatFAOData(fao_data)
+
+    # Step two: Get woah data
+    if country == "USA":
+        woah_data = woah.get_data("United%20States%20of%20America", specie)
+    else:
+        woah_data = woah.get_data(country, specie)
+
+    woah_data = woah.formatWoahData(woah_data)
+
+    # Step 3: Get Census data
+    csv_data, csv_index_list, species = API_helpers.helperFunctions.getFormattedCensusData(country, specie, species)
+
+    # Step 4: Get National data
+    nationalData, nationalData_index_list, species = API_helpers.helperFunctions.getFormattedNationalData(country, specie, species)
+
+    # Build a master dataframe
+    master_df = pd.concat([fao_data, woah_data, csv_data.iloc[csv_index_list], nationalData.iloc[nationalData_index_list]])
+
+    # Build the plotly graph
+    fig = px.line(master_df,
+                x=master_df["year"],
+                y=master_df["population"],
+                color=master_df["source"],
+                markers=True)
+
+    # Build out the polynomial regression lines
+    polyDegree = int(polyDegree)
+    x = []
+    if source != "No Options Available":
+        for i in range(1, polyDegree+1):
+            #Turn the data into a numpy array
+            if source == "FAOSTAT":
+                x = fao_data["year"]
+                y = fao_data["population"]
+
+            elif source == "WOAH":
+                x = woah_data["year"]
+                y = woah_data["population"]
+
+            elif source == "UN Census Data":
+                x = csv_data.iloc[csv_index_list]["year"]
+                y = csv_data.iloc[csv_index_list]["population"]
+
+            else:
+                x = nationalData.iloc[nationalData_index_list]["year"]
+                y = nationalData.iloc[nationalData_index_list]["population"]
+
+            #Convert to numpy arrays
+            x = np.array(x)
+            y = np.array(y)
+
+            #Fit the model
+            model = PolynomialFeatures(degree=i)
+            x_new = model.fit_transform(x.reshape(-1, 1))
+            new_model = LinearRegression()
+            new_model.fit(x_new, y)
+
+            #Add in any missing years
+            for i in range(1960, x[-1]+1):
+                if i not in x:
+                    x = np.append(x, i)
+
+            x.sort()
+
+            #Add in the predicted value years
+            print("x[-1]: ", x[-1])
+            print(x)
+            x = np.append(x, [i for i in range(x[-1] + 1, maxYear+1)])
+            x_new = model.fit_transform(x.reshape(-1, 1))
+            ypredict = new_model.predict(x_new)
+
+            fig.add_traces(go.Scatter(x=x, y=ypredict, mode='lines', name='Polynomial Regression Degree ' + str(i)))
+
+    fig.update_layout(
+        title=f"Population of {specie} in {country}",
+        xaxis_title="Year",
+        yaxis_title="Population",
+        legend_title="Sources",
+        font= dict(
+            size = 18,
+            color = "black"
+        ),
+        plot_bgcolor='white',
+        xaxis = dict(
+        tickmode='array',
+        tickvals = x,
+        ticktext = x,
+    ),
+    )
+
+    fig.update_yaxes(
+        type='linear',
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='black',
+        gridcolor='lightgrey'
+    )
+
+    fig.update_xaxes(
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='black',
+        gridcolor='lightgrey'
+    )
+
+    fig.update_traces(line=dict(width=5))
+
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
